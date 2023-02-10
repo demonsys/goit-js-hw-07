7211bd6cf4745bc54ca08284c165b0b7b3703cd0
demonsys <demonsysk@gmail.com>
2023-02-10 20:54:06 +0200

    дз2+

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,10 +2,21 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
-const gallery = document.querySelector(".gallery");
-gallery.innerHTML = galleryItems.reduce(
-  (html, current) =>
-    html +
-    `<a class="gallery__item" href="${current.original}"> <img src="${current.preview}" alt="${current.description}" class="gallery__image" /></a> `,
-  ""
-);
+const galleryEl = document.querySelector(".gallery");
+// galleryEl.innerHTML = galleryItems.reduce(
+//   (html, current) =>
+//     html +
+//     `<a class="gallery__item" href="${current.original}"> <img src="${current.preview}" alt="${current.description}" class="gallery__image" /></a> `,
+//   ""
+// );
+galleryEl.innerHTML = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href='${original}'> <img src='${preview}' alt='${description}' class="gallery__image" /></a> `
+  )
+  .join("");
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
